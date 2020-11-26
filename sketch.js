eb67925d8647fc678ed5c5774856537b03f0201f
IhaveNoFriends12369 <72689688+IhaveNoFriends12369@users.noreply.github.com>
2020-11-26 19:42:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+
+
+
+
+
+
+function draw() {
+  background(220);
+  text("How to make cheese rolls",10,50);
+   text("get cheese and cut it into pieces and make sure not to eat it",10,70);
+   text("get some dough and flatten it",10,100);
+   text("cut it and slap the pieces of cheese in it and roll it over",10,130);
+   text("deep fry it and make them crispy black",10,160);
+  text("put it in a plate and serve them to the people you hate",10,190);
+  
+  text("note: I did it like this because it didn't know what I was doing with gitbash",10,50);
+  
+  
+  
+  
+  
+}
